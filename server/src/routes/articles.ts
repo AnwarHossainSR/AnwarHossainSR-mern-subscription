b756071689530b8c37d30e63ecb3b91a95932330
Anwar Hossain <mahedianwar@gmail.com>
2022-10-16 16:36:08 +0600

    Implmented payment process with Stripe

--- server/src/routes/articles.ts
+++ server/src/routes/articles.ts
@@ -1,20 +1,19 @@
-import express from "express";
-import User from "../models/user";
-import Article from "../models/article";
-import { checkAuth } from "../middleware/checkAuth";
-import { stripe } from "../utils/stripe";
+import express from 'express';
+import { checkAuth } from '../middleware/checkAuth';
+import Article from '../models/article';
+import User from '../models/user';
+import { stripe } from '../utils/stripe';
 
 const router = express.Router();
 
-router.get("/", checkAuth, async (req, res) => {
+router.get('/', checkAuth, async (req, res) => {
   const user = await User.findOne({ email: req.user });
-  console.log(user?.stripeCustomerId);
 
   const subscriptions = await stripe.subscriptions.list(
     {
       customer: user?.stripeCustomerId,
-      status: "all",
-      expand: ["data.default_payment_method"],
+      status: 'all',
+      expand: ['data.default_payment_method'],
     },
     {
       apiKey: process.env.STRIPE_SECRET_KEY,
@@ -26,20 +25,24 @@ router.get("/", checkAuth, async (req, res) => {
   //@ts-ignore
   const plan = subscriptions.data[0].plan.nickname;
 
-  if (plan === "Basic") {
-    const articles = await Article.find({ access: "Basic" });
+  console.log(plan);
+  
+
+  if (plan === 'Basic') {
+    console.log('Basic');
+    const articles = await Article.find({ access: 'Basic' });
     return res.json(articles);
-  } else if (plan === "Standard") {
+  } else if (plan === 'Standard') {
+    console.log('Standard');
     const articles = await Article.find({
-      access: { $in: ["Basic", "Standard"] },
+      access: { $in: ['Basic', 'Standard'] },
     });
     return res.json(articles);
   } else {
+    console.log('Premium');
     const articles = await Article.find({});
     return res.json(articles);
   }
-
-  res.json(plan);
 });
 
 export default router;
